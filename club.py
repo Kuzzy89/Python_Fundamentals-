c0953bceaa05211b781902b8b11cc4c92228ecca
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-05-07 06:56:29 +0300

    Create club.py

---
+++ club.py
@@ -0,0 +1,11 @@
+profit = int(input())
+command = input()
+coctails_price = 0
+while command != "Party!":
+    coctails = len(command)
+    count_coctails = int(input())
+
+if command == "Party":
+    print(f"We need {недостигаща сума} leva more.")
+
+    command = input()
