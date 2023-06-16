5e30e777131e380dec60030ab98ec09effc6a551
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-06-16 07:07:21 +0300

    Create 07. Min Max and Sum.py

---
+++ 07. Min Max and Sum.py
@@ -0,0 +1,9 @@
+num = [int(el) for el in input().split()]
+
+mini = min(num)
+maxs = max(num)
+sums = sum(num)
+
+print(f"The minimum number is {mini}")
+print(f"The maximum number is {maxs}")
+print(f"The sum number is: {sums}")
