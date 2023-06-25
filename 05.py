8394eb9f9534acb651f942bd731533fd6e8d0ffc
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-06-25 06:42:21 +0300

    Create 05.py

---
+++ 05.py
@@ -0,0 +1,11 @@
+sea_trip_count = int(input())
+mountain_trip_count = int(input())
+
+sea_trip = 680
+mountain_trip = 499
+
+all_sum = (sea_trip_count * sea_trip) + (mountain_trip_count * mountain_trip)
+pack_sea = sea_trip_count
+pack_mount = mountain_trip_count
+
+while
